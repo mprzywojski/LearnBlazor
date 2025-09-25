fc1eaba168c29ed418cd4eefd176ba4b75d8cc98
Michał Przywojski <michal.przywojski@intelo.pl>
2025-09-25 16:47:42 +0200

    Initial commit

---
+++ LearnBlazor/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace LearnBlazor.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async Task ToastrSuccess(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("showToastr", "error", message);
+        }
+    }
+}
---
+++ LearnBlazor/wwwroot/js/common.js
@@ -0,0 +1,30 @@
+﻿window.showToastr = function (type, message) {
+    if (type === 'success') {
+        toastr.success(message);
+    } else if (type === 'error') {
+        toastr.error(message);
+    } else if (type === 'info') {
+        toastr.info(message);
+    } else if (type === 'warning') {
+        toastr.warning(message);
+    }
+}
+
+window.showSwal = function (type, message) {
+    if (type == "success") {
+        Swal.fire({
+            title: 'Good job!',
+            text: message,
+            icon: 'success',
+            confirmButtonText: 'Cool'
+        });
+    }
+    else {
+        Swal.fire({
+            title: 'Task failed!',
+            text: message,
+            icon: 'error',
+            confirmButtonText: 'Cool'
+        });
+    }
+}
